709fe9e04bb79b4a7fd9a8cb714527e1747ff1bb
Lars Lillo Ulvestad <lars.lillo@gmail.com>
2019-12-01 14:47:09 +0100

    Clip path around profile image

--- src/pages/om/biografi.tsx
+++ src/pages/om/biografi.tsx
@@ -2,8 +2,9 @@ import React from "react"
 import AboutMeTemplate from "../../templates/aboutMeTemplate"
 import { AboutHeader } from "../../elements/Headers"
 import styled from "@emotion/styled"
-import { babyBlue, darkestGreen, blackishGreen } from "../../utilities/Colors"
+import { babyBlue } from "../../utilities/Colors"
 import { useImmer } from "use-immer"
+import { mq } from "../../elements/MediaQuery"
 const lilloPhoto = require("../../images/lillo-photo.png")
 const lilloPixel = require("../../images/lillo-pixel.png")
 const blueBorder = require("../../images/border-blue.png")
@@ -16,36 +17,30 @@ export default function AboutPage1() {
     <AboutMeTemplate nextLink={nextLink} prevLink={prevLink}>
       <BiographyContainer>
         <div className="text">
+          <img
+            src={lilloImg}
+            className="image"
+            onMouseEnter={() => setLilloImg(() => lilloPhoto)}
+            onMouseLeave={() => setLilloImg(() => lilloPixel)}
+            alt=""
+          />
+          <div style={{ height: "40px" }} />
           <AboutHeader>Arbeidsbakgrunn</AboutHeader>
           <p>
             Jeg startet arbeidslivet som webjournalist og web-basert
-            kommunikasjonsrådgiver, men skiftet beite da jeg innså hvor givende
-            det er å utvikle apper og nettsider. Jeg jobber som konsulent for{" "}
-            <a href="https://kantega.no">Kantega</a>. Jobben utfordrer meg
-            konstant, og jeg opplever at jeg hele tiden vokser i rollen.
+            kommunikasjonsrådgiver. Nå utvikler jeg apper og nettsider. Jeg
+            jobber som konsulent for <a href="https://kantega.no">Kantega</a>.
           </p>
           <AboutHeader>Foretrukne teknologier</AboutHeader>
           <p>
-            Jeg syns det er spennende å lære nye språk og frontendrammeverk, men
-            jeg har noen favoritter.
+            Det funksjonelle språket Elm gir pålitelig kode for webapper som
+            krever mindre vedlikehold. Det kommer også kunden til gode.
           </p>
           <p>
-            Jeg foretrekker det funksjonelle språket Elm der det er rom for det.
-            Hovedgrunnen er mer pålitelig kode som krever mindre vedlikehold.
-            Det kommer også kunden til gode.
-          </p>
-          <p>
-            Når det kommer til blogger og statiske sider syns jeg React/Gatsby
-            med TypeScript er dynamitt.
+            På blogger og statiske sider velger jeg ofte GatsbyJS med React og
+            TypeScript.
           </p>
         </div>
-        <img
-          src={lilloImg}
-          className="image"
-          onMouseEnter={() => setLilloImg(() => lilloPhoto)}
-          onMouseLeave={() => setLilloImg(() => lilloPixel)}
-          alt=""
-        />
       </BiographyContainer>
     </AboutMeTemplate>
   )
@@ -55,13 +50,13 @@ const BiographyContainer = styled.div`
   padding: 1rem;
   font-size: 2rem;
   background: rgb(0, 0, 0);
+  display: flex;
   background: linear-gradient(
     180deg,
     rgba(0, 0, 0, 1) 0%,
     rgba(0, 12, 32, 1) 100%
   );
-  display: grid;
-  grid-template: "text image" 1fr / 1fr auto;
+
   a {
     color: ${babyBlue};
     text-shadow: 1px 1px 0px #0c2c64, -1px -1px 0px #0c2c64,
@@ -70,15 +65,29 @@ const BiographyContainer = styled.div`
     text-decoration: none;
   }
 
-  .text {
-    grid-area: text;
-  }
-
   .image {
-    grid-area: image;
-    width: 80%;
+    ${mq[1]} {
+      float: right;
+      padding: 3rem;
+      padding-right: 0;
+    }
+    width: 100%;
     max-width: 500px;
-    border-width: 28px;
-    border: 15px solid #65cdbd;
+
+    shape-outside: polygon(
+      30.4% 115px,
+      45.99% 8.82%,
+      71.59% 9%,
+      87.93% 20.42%,
+      88.25% 49.84%,
+      78.8% 71.4%,
+      100.28% 90.32%,
+      100.27% 99.86%,
+      -1.2% 99.8%,
+      -0.81% 94.89%,
+      31.38% 78.87%,
+      116px 48.4%,
+      29.49% 37.04%
+    );
   }
 `
