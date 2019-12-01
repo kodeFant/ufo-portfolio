9641d1e679a767d7d22146b5ca53a7cfb8af2f35
Lars Lillo Ulvestad <lars.lillo@gmail.com>
2019-12-01 15:55:08 +0100

    Prevent sound playing on image hover

--- src/components/ClickSound.tsx
+++ src/components/ClickSound.tsx
@@ -1,14 +1,17 @@
 import React, { useRef, useEffect } from "react"
+import { useImmer } from "use-immer"
 const clickSound = require("../sounds/click.mp3")
 
 export default function ClickSound() {
   const audio = useRef<HTMLAudioElement>(null)
+  const [played, setPlayed] = useImmer(false)
   useEffect(() => {
-    if (audio.current) {
+    if (audio.current && !played) {
       audio.current.volume = 0.1
       audio.current.play()
+      setPlayed(() => true)
     }
-  })
+  }, [played])
   return (
     <audio ref={audio} id="audio">
       <source src={clickSound} type="audio/mpeg" />
