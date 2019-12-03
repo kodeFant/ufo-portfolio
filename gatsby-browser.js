ecd5453842a1ecfc1c1a44d5b9d5db5b75bb3c35
Lars Lillo Ulvestad <lars.lillo@gmail.com>
2019-12-03 09:43:47 +0100

    Remove service worker

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,3 +7,14 @@
 // You can delete this file if you're not using it
 
 import "typeface-vt323"
+
+self.addEventListener("activate", function(e) {
+  self.registration
+    .unregister()
+    .then(function() {
+      return self.clients.matchAll()
+    })
+    .then(function(clients) {
+      clients.forEach(client => client.navigate(client.url))
+    })
+})
